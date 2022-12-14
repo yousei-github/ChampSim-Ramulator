ca2f66a44f90a8fa5a9bd221a8f42a2f328ce4c1
Yousei <72794107+yousei-github@users.noreply.github.com>
2022-12-14 16:06:57 +0900

    update address of traces

--- scripts/download_dpc3_traces.sh
+++ scripts/download_dpc3_traces.sh
@@ -3,5 +3,5 @@
 mkdir -p $PWD/../dpc3_traces
 while read LINE
 do
-    wget -P $PWD/../dpc3_traces -c http://hpca23.cse.tamu.edu/champsim-traces/speccpu/$LINE
+    wget -P $PWD/../dpc3_traces -c https://dpc3.compas.cs.stonybrook.edu/champsim-traces/speccpu/$LINE
 done < dpc3_max_simpoint.txt
